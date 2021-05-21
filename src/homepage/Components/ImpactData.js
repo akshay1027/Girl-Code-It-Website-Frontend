060e1fe80ff81e9416e2006d8538d11bb2330d89
akshay1027 <akshayar1027@gmail.com>
2021-05-21 11:57:25 +0530

    changed

--- src/homepage/Components/ImpactData.js
+++ src/homepage/Components/ImpactData.js
@@ -1,19 +1,19 @@
 var ImpactData = [
   {
     id: 1,
-    name: "Opportunity Calendar",
-    imgUrl: "image1.svg",
-    para1: "Know right Opportunities",
-    para2: "at right time!",
-    link: "/oppcalendar"
-  },
-  {
-    id: 2,
     name: "CodeMaps",
     imgUrl: "image2.svg",
     para1: "Awesome roadmaps to learn",
     para2: "to code the right way!",
     link: "/codemaps"
+  },
+  {
+    id: 2,
+    name: "Opportunity Calendar",
+    imgUrl: "image1.svg",
+    para1: "Know right Opportunities",
+    para2: "at right time!",
+    link: "/oppcalendar"
   },
   // {
   //   id: 3,
